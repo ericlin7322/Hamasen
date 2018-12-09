8327ecb0050c59ff3fa7f9616498dcf5ed42c52f
Eric Lin <eric20706@gmail.com>
2018-12-09 18:49:01 +0800

    Initial Commit

---
+++ Hamasen/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Hamasen
+//
+//  Created by Eric Lin on 2018/12/9.
+//  Copyright © 2018 Eric Lin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
